a30733e59749db31021d7d9a2714dced4281a1be
shrinemtg <shrine.mtg@gmail.com>
2023-06-28 01:15:27 +0900

    途中経過表裏の実装

--- src/components/trump.tsx
+++ src/components/trump.tsx
@@ -1,46 +1,28 @@
 "use client";
 import styled from "styled-components";
 import Image from "next/image";
-import { use } from "react";
+import React, { useState } from "react";
+
 export default function Trump() {
+  const [imageSrc, setImageSrc] = useState("/img/card_back.png");
+  // 1. useStateフックを使って状態変数imageSrcを定義し、初期値を"/img/card_back.png"に設定する
+
+  // 3. 画像がクリックされたときに実行される関数handleClickを定義する
+  const handleClick = () => {
+    // 3.1 setImageSrc関数を呼び出してimageSrcの値を"/img/another_image.png"に変更する
+    setImageSrc("/img/joker_1.png");
+  };
   return (
     <TrumpContainer>
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((number) => (
-        <>
-          {/* clubs */}
-          <Image
-            src={`/img/${number}_of_clubs.png`}
-            alt=""
-            layout="responsive"
-            width={409}
-            height={600}
-          />
-          {/* diamonds */}
-          <Image
-            src={`/img/${number}_of_diamonds.png`}
-            alt=""
-            layout="responsive"
-            width={409}
-            height={600}
-          />
-          {/* hearts */}
-          <Image
-            src={`/img/${number}_of_hearts.png`}
-            alt=""
-            layout="responsive"
-            width={409}
-            height={600}
-          />
-          {/* spades */}
-          <Image
-            src={`/img/${number}_of_spades.png`}
-            alt=""
-            layout="responsive"
-            width={409}
-            height={600}
-          />
-        </>
-      ))}
+      <Image
+        src={imageSrc}
+        alt=""
+        layout="responsive"
+        width={409}
+        height={600}
+        onClick={handleClick} // 4.2. クリックイベントを処理する関数handleClickを指定する
+        style={{ cursor: "pointer" }} // 4.3. カーソルのスタイルを変更する
+      />
     </TrumpContainer>
   );
 }
@@ -51,11 +33,4 @@ const TrumpContainer = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
-`;
-
-const CardImage = styled(Image)`
-  width: 25%; /* 画面幅に対しての割合で調整 */
-  height: auto; /* 幅に合わせて高さを自動調整 */
-  max-width: 100%; /* 最大幅を指定 */
-  max-height: 100%; /* 最大高さを指定 */
 `;
